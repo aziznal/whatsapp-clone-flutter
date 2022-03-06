7ab95e8185fcaa491b47063cd7238136365d0ce4
aziznal <aziznal.dev@gmail.com>
2022-03-06 21:25:44 +0300

    refactor: remove unused import

--- lib/src/modules/main_chat_list/widgets/chat_list/chat_list.dart
+++ lib/src/modules/main_chat_list/widgets/chat_list/chat_list.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:get/get.dart';
-
 import 'package:com.aziznal.whatsapp_clone/src/modules/common/mock/mock_data.dart';
 
 import 'package:com.aziznal.whatsapp_clone/src/modules/main_chat_list/widgets/chat_list/chat_item.dart';
