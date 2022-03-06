d3cb14ea9a25d913127e5a0f392a4cfccb8dfd30
aziznal <aziznal.dev@gmail.com>
2022-03-06 07:37:26 +0300

    refactor: format mock_data file to look better

--- lib/src/modules/common/mock/mock_data.dart
+++ lib/src/modules/common/mock/mock_data.dart
@@ -2,13 +2,28 @@ import 'package:com.aziznal.whatsapp_clone/src/modules/common/models/chat_item.d
 
 class MockData {
   static List<ChatItem> chats = [
-    ChatItem(title: "John Doe", subtitle: "Wake me up"),
-    ChatItem(title: "Eva", subtitle: "Wake me up inside"),
-    ChatItem(title: "Sam Spa", subtitle: "Can't wake up"),
-    ChatItem(title: "Fa Koffman", subtitle: "Wake me up inside"),
-    ChatItem(title: "Isyurf ridge", subtitle: "Save meeeee"),
     ChatItem(
-        title: "Runni nggogetit",
-        subtitle: "Call my name and save me from the dark"),
+      title: 'John Doe',
+      subtitle: 'Wake me up',
+    ),
+    ChatItem(
+      title: 'Eva',
+      subtitle: 'Wake me up inside',
+    ),
+    ChatItem(
+      title: 'Sam Spa',
+      subtitle: "Can't wake up",
+    ),
+    ChatItem(
+      title: 'Fa Koffman',
+      subtitle: 'Wake me up inside',
+    ),
+    ChatItem(
+      title: 'Isyurf ridge',
+      subtitle: 'Save meeeee',
+    ),
+    ChatItem(
+        title: 'Runni nggogetit',
+        subtitle: 'Call my name and save me from the dark'),
   ];
 }
